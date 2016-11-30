56233ccb121548387553fe23c283634493c3ffe5
Sami Nieminen <sami@nieminen.fi>
2016-11-30 20:10:00 +0200

    removed remainings of calendar source

--- app/calendar/CalendarStore.js
+++ app/calendar/CalendarStore.js
@@ -1,13 +1,10 @@
 import alt from '../alt';
 import CalendarActions from './CalendarActions';
-import CalendarSource from './CalendarSource.js';
 
 class CalendarStore {
   constructor() {
     this.bindActions(CalendarActions);
     this.events = [];
-
-    this.exportAsync(CalendarSource);
   }
 
   onGetCalendarEventsSuccess(data) {
